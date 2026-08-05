4f9b0b3de14d41e35f20085cc9a0f24d06b93922
Nolan Powell <nolanhpowell.2@gmail.com>
2026-08-05 09:47:41 -0500

    Added pose detection and added landmarks to the feed

--- main.py
+++ main.py
@@ -9,12 +9,34 @@ mp_pose = mp.solutions.pose
 
 # Setup Video Feed
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('Mediapipe Feed', frame)
+# Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = Falseresults = pose.process(image)
 
-cap.release()
-cv2.destroyAllWindows()
+        # Make detection
+        results = pose.process(image)
+
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                # Recolor and change drawing landmarks and connections
+                                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                  )
+
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
